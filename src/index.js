415f5ed07d5af9f226241fd83b75da7b329df499
matttm <matttmaloney@gmail.com>
2023-07-29 13:12:03 -0400

    creating fmi from event

--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
+const { FileMetaInfo } = require("storage-db/src");
+
 export const handler = async (event) => {
-  const eventName = event.Records[0].eventName;
+  const record = event.Records[0];
+  const eventName = record.eventName;
+  const bucketName = record.s3.bucket.name;
+  const obj = record.object;
+  //
+  // The key will begin with the userId followed by a semicolon
+  //
+  // i.e. 209:test.pdf
+  //        hence 209 is the user id
+  //
+  const userId = obj.key.split(":")[0];
+  switch (eventName) {
+    case "ObjectCreated:Put":
+      console.log("Object stored in S3");
+      await FileMetaInfo.create({
+        fileTypeCd: 1,
+        userId,
+        fileSizeKb: obj.size,
+        s3BucketName: bucketName,
+        s3ObjectName: obj.key,
+        s3ObjectKey: obj.key,
+      });
+      break;
+    default:
+      break;
+  }
   const response = {
     statusCode: 200,
-    body: JSON.stringify('Hello from Lambda!'),
+    body: JSON.stringify("Hello from Lambda!"),
   };
   return response;
 };
